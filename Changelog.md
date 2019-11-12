208330af5ad24a9abe4d4d085816af6daff37bf3
kynite <54864582+kynite@users.noreply.github.com>
2019-11-12 13:34:33 -0600

    Updated Changelog.md and main.py
    Updated changelog to show all the changes that occured in the program. Added user input and menus in main.py, also organized the program in functions and made the RPG play continously.

--- Changelog.md
+++ Changelog.md
@@ -6,3 +6,9 @@
 - Created Outline.txt
 - Basic story created
 - Basic game idea created
+
+## Version 0.1 Alpha
+- Game menus added
+- Organized menus, characters, inventory, and locations in functions
+- User input added
+- Quit option added
--- main.py
+++ main.py
@@ -1,25 +1,176 @@
 # CS 30
 # Period 4
-# Date : 10/22/2019
+# Date : 11/12/2019
 # Krutik Rana
-# Program description : Creating dictionaries for the game
+# Program description : Orginizing the game into functions
 
-# Prints out a statement telling user a list of characters is upcoming
-print("These are the characters:")
+
+def friendly_characters():
+    """Loops for friendly character in the game"""
+    # Special for loop that lists the friendly character in the game
+    for k, v in characters.items():
+        if k == "Xunerophore":
+            print(f"The main character is {k} with {v}HP")
+        elif k == "Shopkeeper":
+            print(f"The {k} is {v}")
+
+
+def enemies_list():
+    """Loop for enemies in the game"""
+    # Special loop that lists the enemies in the game
+    for k, v in enemies.items():
+        print(f"{k} is an enemy with {v['HP']}HP and {v['Attack']} Attack\
+        Damage")
+
+
+def inventory():
+    """Commands to access the characters inventory"""
+    # While loop to continously play
+    while True:
+        # Print statement detailing how to go to the last menu
+        print('\ntype q to go back to previous menu')
+        # Aquiring user input for what they want to access of the inventory
+        user = input('action: ')
+        # Making the user input all lower case to match if and elif statements
+        user = user.lower()
+        # Checks to see if the user typed weapon
+        if user == 'weapon':
+            # Loop that lists out weapons and their stats
+            for k, v in weapons_inventory.items():
+                print(f"In your weapon stash you have a {k} that deals {v} Dam\
+age")
+        # Checks to see if user typed fishing rod
+        elif user == 'fishing rod':
+            # Loop that lists out fishing rods in inventory and their stats
+            for k in fishingrod_inventory:
+                print(f"\nIn your fishing rod stash you have a {k}:")
+                for v in fishingrod_inventory[k]:
+                    print(f"This fishing rod can capture {v}")
+        # Checks to see if user typed heal
+        elif user == 'heal':
+            # Loop that lists out health items in inventory
+            for k, v in healthitems_inventory.items():
+                print(f"In your backpack you are carrying {k} that heals\
+ for {v}HP")
+        # Checks to see if user typed q
+        elif user == 'q':
+            # Quits this part of the menu
+            break
+        # Checks to see if the user typed anything else
+        else:
+            # Tells user it is an invalid option
+            print('Invalid option')
+
+
+def location():
+    """Commands that allow you to travel to different locations"""
+    # While loop for continous play
+    while True:
+        # Tells user to type q to go to the previous menu
+        print('\ntype q to go back to previous menu')
+        # Asks for user input of what location they would like to go to
+        user = input('select location: ')
+        # Makes all user inputs lower cased to match if and elif statements
+        user = user.lower()
+        # Checks to see if user typed in specific location
+        if user == 'hooligan lake':
+            # prints out that they are traveling to that location
+            print('traveling to Hooligan lake')
+        # Checks to see if user typed in specific location
+        elif user == 'where are we now lake':
+            # prints out that they are traveling to that location
+            print('traveling to Where are we now lake')
+        # Checks to see if user typed in specific location
+        elif user == 'ethereal lake':
+            # prints out that they are traveling to that location
+            print('traveling to Ethereal lake')
+        # Checks to see if user typed in q
+        elif user == 'q':
+            # Quits this part of the menu
+            break
+        # Checks to see if user typed in specific location
+        elif user == 'residential lake':
+            # prints out that they are traveling to that location
+            print('traveling to Residential Lake')
+        # Checks to see if user typed anything else
+        else:
+            # Tells user it is an invalid option
+            print('invalid option')
+
+
+def ingame_menu():
+    """The menu that appears after the main menu/the first menu"""
+    # While loop for continous play
+    while True:
+        # Tells user that typing q goes to the main menu
+        print('type q to go back to the main menu')
+        # Asks for user input
+        menu = input('type location or inventory: ')
+        # Makes all the user input into lower case
+        menu = menu.lower()
+        # Checks to see if user typed inventory
+        if menu == 'inventory':
+            # Tells user to type in certain commands to access further menus
+            print('type [weapon], [fishing rod], [heal]')
+            # Runs the inventory function
+            inventory()
+        # Checks to see if user typed location
+        elif menu == 'location':
+            # A loop that lists out the locations
+            for k in locations:
+                print(f"\nTravel to {k}?")
+                for v in locations[k]:
+                    print(f"This Lake contains {v}")
+            # Runs the location function
+            location()
+        # Checks to see if user typed in q
+        elif menu == 'q':
+            # Goes to main menu
+            break
+        # Checks to see if anything else was typed in
+        else:
+            # Tells user that was an invalid selection
+            print('invalid select again')
+
+
+def main_menu():
+    """The main menu of the game, the first one you see"""
+    # While loop for continous play
+    while True:
+        # Prints out name of the game
+        print('Ethereal Ultimatum')
+        # Just something to seperate the title
+        print('----------------')
+        # Informs user to type y to play the game
+        print('type y to play the game')
+        # Informs user to type q to quit the game
+        print('type q to quit the game')
+        # Gets user input
+        selection = input('Make your selection: ')
+        # Converts user input into all lower case
+        selection = selection.lower()
+        # Checks to see if y is typed
+        if selection == 'y':
+            # Runs the ingame menu function
+            ingame_menu()
+        # Checks to see if q is typed
+        elif selection == 'q':
+            # Ends the game
+            break
+            # Tells user game has ended
+            print('game ended')
+        # Checks to see if anything else is typed
+        else:
+            # Tells user it is an invalid option
+            print('Invalid option')
+
+
 # Dictionary of friendly characters with values
 characters = {
     "Xunerophore": "100", "Shopkeeper": "the person you can buy\
  items from."
              }
-# A special loop with different lines for friendly characters
-for k, v in characters.items():
-    if k == "Xunerophore":
-        print(f"The main character is {k} with {v}HP")
-    elif k == "Shopkeeper":
-        print(f"The {k} is {v}")
 
-print("\n")
-print("These are the enemies:")
 # Dictionary of Enemies with values
 enemies = {'Salmon': {'HP': 10, 'Attack': 2},
            'GoldFish': {'HP': 5, 'Attack': 1},
@@ -29,14 +180,7 @@ enemies = {'Salmon': {'HP': 10, 'Attack': 2},
            'Saw Shark': {'HP': 25, 'Attack': 6},
            'Ethereal Ultimatum': {'HP': 1000, 'Attack': 20}
            }
-# A Loop created for the enemies
-for k, v in enemies.items():
-    print(f"{k} is an enemy with {v['HP']}HP and {v['Attack']} Attack Damage")
 
-# Prints out a bunch of dashed lines to seperate characters and inventory
-print("----------------------------------------------------------------------")
-# Prints text stating this is the inventory
-print("This is the inventory:")
 # Creates a dictionary of weapons
 weapons_inventory = {'Stick': 1, 'Bat': 3, 'Spiked Bat': 5, 'Sword': 10}
 # Creates a dictionary with a list of uses for the fishing rods
@@ -49,41 +193,13 @@ fishingrod_inventory = {'Basic rod': ['Salmon', 'GoldFish', 'Guppy'],
 healthitems_inventory = {'Sandwich': 20, 'Medkit': 50, 'Pills': '5',
                          'Grass': 10
                          }
-# A print statement telling you its listing the weapons in the inventory
-print("(Weapons)")
-# A loop for all the weapons in the weapons inventory also stating the damage
-for k, v in weapons_inventory.items():
-    print(f"In your weapon stash you have a {k} that deals {v} Damage")
-# Prints a seperating line
-print("\n")
-# A print statement telling you its listing the fishing rods in the inventory
-print("(Fishing Rods)")
-# A loop that loops through each rod and what fish you can capture with it
-for k, v in fishingrod_inventory.items():
-    for n in range(len(v)):
-        print(f"In your fishing rod stash you have a {k} that allows you to \
-capture {v[n]}")
 
-# Prints a seperating line
-print("\n")
-# A print statement telling you its listing the health items in the inventory
-print("(Health Items)")
-# A loop that goes through each health item and how much the item heals for
-for k, v in healthitems_inventory.items():
-    print(f"In your backpack you are carrying {k} that heals for {v}HP")
-
-# Prints out a bunch of dashed lines to seperate inventory and locations
-print("----------------------------------------------------------------------")
-# Prints text stating these are the locations
-print("These are the locations:")
 # Creates a dictionary with a list of fishes available at each lake
 locations = {'Hooligan Lake': ['Salmon', 'GoldFish', 'Guppy'],
              'Where are we now Lake': ['Hammerhead Sharks', 'The Great White',
                                        'Saw Sharks'],
              'Ethereal Lake': ['Ethereal Ultim\
-atum']
+atum'], 'Residential Lake': ['Shopkeeper'],
              }
-# A loop that loops through each lake and what fish are at the lake
-for k, v in locations.items():
-    for n in range(len(v)):
-        print(f"Travel to {k}? This Lake contains {v[n]}")
+# Calls main menu function
+main_menu()
